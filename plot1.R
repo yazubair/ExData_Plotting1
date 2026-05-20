9b9c5a6d62678aa00c446149f5817e87ed7fd0e2
yazubair <yazubair@yahoo.com>
2026-05-20 16:30:44 -0400

    Add files via upload

--- plot1.R
+++ plot1.R
@@ -2,9 +2,9 @@ hpc <- read.table("C:/Users/yazubair/Documents/Coursera/R/Data Science  Foundati
 
 fhpc <- subset(hpc, hpc$Date == "1/2/2007"|hpc$Date == "2/2/2007")
 
-GAP <- as.numeric(hpc$Global_active_power)
+agap <- as.numeric(as.character(hpc$Global_active_power))
 
-hist(GAP, main = "Global Active Power", xlab = "Global Active Power (kilowatts)",  col ="RED")
+hist(agap, main = "Global Active Power", xlab = "Global Active Power (kilowatts)",  col ="RED", ylim = c(0,1200))
 
 dev.copy(png, file = "plot1.png", height = 480, width = 480)
 dev.off()
